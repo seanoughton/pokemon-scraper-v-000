ac9366bd9a04357fdb36866cc4ef1718d0203c54
Sean Oughton <seanoughton@gmail.com>
2018-03-08 21:20:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
     Pokemon.new(id:pokemon_array[0],name:pokemon_array[1],type:pokemon_array[2],db:database_connection)
   end
 
-  #ALTER TABLE cats ADD COLUMN breed TEXT;
+  #ALTER TABLE cats ADD COLUMN breed TEXT SET DEFAULT 'Sandnes';
 
 
 end
