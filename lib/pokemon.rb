56a768079fd2f64f31d91e57600acae44cb15d73
Sean Oughton <seanoughton@gmail.com>
2018-03-08 22:21:24 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,7 +25,8 @@ class Pokemon
     #self.hp = new_hp
     #database_connection.execute("ALTER TABLE pokemon VALUE(?)",new_hp)
     #UPDATE characters SET species = "Martian" WHERE id = 8;
-    pokemon_instance.db.execute("UPDATE pokemon SET hp=? WHERE id=?",new_hp, pokemon_instance.id)
+    database_connection = pokemon_instance.db
+    database_connection.execute("UPDATE pokemon SET hp=? WHERE id=?",new_hp, pokemon_instance.id)
   end
 
 
