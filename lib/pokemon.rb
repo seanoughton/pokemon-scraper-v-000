b361b732e2744c06311be0b2ec9ab91a5454322a
Sean Oughton <seanoughton@gmail.com>
2018-03-08 20:53:46 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
 
   def self.find(number,database_connection)
     #database_connection.execute("SELECT * FROM pokemon WHERE id=?", number)
-    pokemon_info = database_connection.execute("SELECT * FROM pokemon WHERE id=?", number).flatten
+    pokemon_info_array = database_connection.execute("SELECT * FROM pokemon WHERE id=?", number).flatten
     Pokemon.new(id: nil,name: nil,type: nil ,db:database_connection)
   end
 
