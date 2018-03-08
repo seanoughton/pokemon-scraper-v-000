00722eec4acd4550f98ef2b0d22950d85348f1fa
Sean Oughton <seanoughton@gmail.com>
2018-03-08 20:15:42 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,10 @@
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :name, :type, :db
 
 
 
-  def initialize(id: nil, name: nil, type: nil, db: nil)
+  def initialize(name: nil, type: nil, db: nil)
 
   end
 
