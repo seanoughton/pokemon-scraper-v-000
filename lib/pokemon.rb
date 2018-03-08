aba91a9d2f74a13c5784ff8b730c2b5294a773f1
Sean Oughton <seanoughton@gmail.com>
2018-03-08 22:38:48 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
 
   def self.find(number,database_connection)
     pokemon_array = database_connection.execute("SELECT * FROM pokemon WHERE id=?", number).flatten
-    Pokemon.new(id:pokemon_array[0],name:pokemon_array[1],type:pokemon_array[2],db:database_connection,hp:pokemon_array[1])
+    Pokemon.new(id:pokemon_array[0],name:pokemon_array[1],type:pokemon_array[2],db:database_connection,hp:pokemon_array[2])
   end
 
   def alter_hp(new_hp,database_connection)
